341e6a693d217e47241d4a25845972753c2955f3
IDPLive <124122967+IDPLive@users.noreply.github.com>
2025-04-28 15:03:44 +0530

    Update MarketoPlugin.h

--- src/ios/MarketoPlugin.h
+++ src/ios/MarketoPlugin.h
@@ -22,6 +22,5 @@
 - (void)logMessage: (NSString*)msg;
 - (void) getNotification:(CDVInvokedUrlCommand*)command;
 @property(strong, nonatomic) NSString *deeplinkURL;
--(void)getUniqueDeviceID:(CDVInvokedUrlCommand*)command;
 
 @end
